8b9592b5a9601787932f818fb9c736693b2e815c
dishathanki <109351294+dishathanki@users.noreply.github.com>
2026-06-18 22:30:19 +0530

    multer completed

--- DataAssociation2/app.js
+++ DataAssociation2/app.js
@@ -5,15 +5,33 @@ const postModel = require('./models/post');
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const path = require('path');
+const multerconfig = require('./config/multerconfig');
+const upload = require("./config/multerconfig");
 
 app.set("view engine","ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser()); 
+app.use(express.static(path.join(__dirname,"public")))
+
 
 app.get('/',(req,res)=>{
     res.render('index.ejs')
 })
+
+app.get('/profile/upload',(req,res)=>{
+    res.render('profileupload.ejs');
+})
+
+app.post("/upload",isLoggedIn,upload.single("image"),async (req,res)=>{
+    let user = await userModel.findOne({email:req.user.email})
+    user.profilepic = req.file.filename;
+    await user.save();
+    res.redirect("/profile")
+})
+
 
 app.get('/login',(req,res)=>{
     res.render('login.ejs')
